cb9b5f0e03504ba0862f2ed39cebc32af924e0d9
Gerry Steffen <g_steffen@hotmail.com>
2025-07-19 22:08:14 +0100

    env variable lint

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -3,7 +3,11 @@ import { NODE_ENV } from '$env/static/private';
 import { env } from '$env/dynamic/private';
 
 const URI =
-  NODE_ENV === 'production' ? env.DB_PROD_URI : NODE_ENV === 'development' ? env.DB_DEV_URI : env.DB_TEST_URI;
+  NODE_ENV === 'production'
+    ? env.DB_PROD_URI
+    : NODE_ENV === 'development'
+    ? env.DB_DEV_URI
+    : env.DB_TEST_URI;
 
 try {
   await mongoose.connect(URI);
