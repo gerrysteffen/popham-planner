c7cd0387c182eefbd5619590db5438c26d03de58
Gerry Steffen <g_steffen@hotmail.com>
2025-07-19 21:48:08 +0100

    change of env variables

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
-import { DB_URI } from '$env/static/private';
+import { DB_DEV_URI, DB_PROD_URI, DB_TEST_URI, NODE_ENV } from '$env/static/private';
+
+const URI =
+  NODE_ENV === 'production' ? DB_PROD_URI : NODE_ENV === 'development' ? DB_DEV_URI : DB_TEST_URI;
 
 try {
-  await mongoose.connect(DB_URI);
+  await mongoose.connect(URI);
   console.log('Connected successfully to database');
 } catch (err) {
   console.log('Mongoose connection err: ' + err);
